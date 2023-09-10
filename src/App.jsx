c018d5163783cd98559b699254a8f1961de62eb3
rootnure <nur.diu.2791@gmail.com>
2023-09-10 19:55:36 +0600

    Folder Structure & Fetch all countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Countries from './components/Countries/Countries';
 
 function App() {
 
   return (
     <>
       <h2>React World Tour</h2>
+      <Countries></Countries>
     </>
   )
 }
