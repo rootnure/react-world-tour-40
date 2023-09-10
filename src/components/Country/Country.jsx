2d32f396c525d05a9cbf03a0830ede23179d9c54
rootnure <nur.diu.2791@gmail.com>
2023-09-10 20:20:15 +0600

    Toggle state based on click and conditional rendering. onClick, handler function, state all inside same component

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './Country.css';
 const Country = ({country}) => {
     const {name, flags} = country;
-    console.log(country);
+    const [isVisited, setIsVisited] = useState(false);
+
+    const handleVisited = () => {
+        setIsVisited(!isVisited);
+    }
+
     return (
         <div className='country'>
             <h3>{name.common}</h3>
             <img className='max-h-32' src={flags.png} alt={`${name.common} National Flag`} />
+            <br />
+            <button onClick={handleVisited}>Visited</button>
+            <p>{ isVisited ? 'I have visited this country' : 'Want to visit' }</p>
         </div>
     );
 };
