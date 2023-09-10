d40ab313469f31bba6f835a9492aa9738d1b8aa7
rootnure <nur.diu.2791@gmail.com>
2023-09-10 20:54:13 +0600

    Conditional CSS, Conditional Style, Visited List dynamic added, props delegate

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -6,6 +6,8 @@ const Countries = () => {
     
   const [countries, setCountries] = useState([]);
 
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
@@ -14,12 +16,20 @@ const Countries = () => {
     return (
         <>
             <h3>Countries: {countries.length}</h3>
+            <h3>Visited: {visitedCountries.length}</h3>
+            <ul>
+                {
+                    visitedCountries.map((visited, idx) => <li key={idx}>{visited.name.common}</li>)
+                }
+            </ul>
             <div className="country-container">
                 {
                     countries.map(country =>
                         <Country
                             key={country.cca3}
                             country={country}
+                            visitedCountries={visitedCountries}
+                            setVisitedCountries={setVisitedCountries}
                         ></Country>
                     )
                 }
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react';
 import './Country.css';
-const Country = ({country}) => {
-    const {name, flags} = country;
+const Country = ({ country, visitedCountries, setVisitedCountries }) => {
+    const { name, flags } = country;
     const [isVisited, setIsVisited] = useState(false);
 
     const handleVisited = () => {
         setIsVisited(!isVisited);
     }
 
+    const addNewVisited = country => {
+        const newVisited = visitedCountries.map(visited => visited?.name?.common).includes(country?.name?.common) ? [...visitedCountries] : [...visitedCountries, country];
+        setVisitedCountries(newVisited);
+    }
+
     return (
-        <div className='country'>
-            <h3>{name.common}</h3>
+        <div className={`country ${isVisited ? 'visited' : 'not-visited'}`}>
+            <h3 style={{color: isVisited ? 'blue' : 'white'}}>{name.common}</h3>
             <img className='max-h-32' src={flags.png} alt={`${name.common} National Flag`} />
             <br />
             <button onClick={handleVisited}>Visited</button>
-            <p>{ isVisited ? 'I have visited this country' : 'Want to visit' }</p>
+            {isVisited ? 'I have visited this country' : 'Want to visit'}
+            <br />
+            <button onClick={() => addNewVisited(country)}>Add Visited</button>
         </div>
     );
 };
